3c4b434e1260947b985f227552f23f3469bef59d
Rashmi Jadhav <jrashmi853@gmail.com>
2024-05-25 16:24:41 +0530

    signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 
 // function is asyncronous because we are going to wait for some time to get the data from mongodb and give it to the user
@@ -31,3 +32,41 @@ res.json('Signup successful');
 }
 
 };
+
+// next is to handle error
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try{
+        const validUser = await User.findOne({ email });
+        // if user is not found
+        if(!validUser){
+            return next(errorHandler(404, 'User not found'));
+        }
+        // compare passwords
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        // if password is not valid
+        if(!validPassword){
+            return next(errorHandler(400, 'Invalid Password'));
+        }
+
+        // create token - this creates encrypted/hashed password
+        const token = jwt.sign(
+            { id: validUser._id }, process.env.JWT_SECRET //we can also add expiry time. like one hr or if site is closed session is complete. if we dont add anything the session completes as the window is closed.
+        );
+
+        const { password: pass, ...rest } = validUser._doc;
+
+        // add token to create cookie using res
+        res.status(200).cookie('access_token', token, {
+            httpOnly : true,
+        }).json(rest) // this will not show password now and cookie is also created
+        
+    } catch(error){
+        next(error);
+    }
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { signup } from '../controllers/auth.controller.js';
+import { signin } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 // post - user is giving information
 router.post('/signup', signup);
+router.post('/signin', signin)
 
 export default router;
